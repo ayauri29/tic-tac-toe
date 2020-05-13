bd88f219e03223147882bde3aca61c663d289b9a
ayauri29 <albayauri.29@gmail.com>
2020-05-13 17:00:08 -0500

    Updates

---
+++ precache-manifest.545fb634b95343149cd1105410199b79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e0e60f7876d3a10529fc8a2c0921a6",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ee9c2efcfe40cb9ec500",
+    "url": "/tic-tac-toe/static/css/main.68a282b9.chunk.css"
+  },
+  {
+    "revision": "659c0a7a2e8c64e1027d",
+    "url": "/tic-tac-toe/static/js/2.0c1511cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.0c1511cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9c2efcfe40cb9ec500",
+    "url": "/tic-tac-toe/static/js/main.ce94533b.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
